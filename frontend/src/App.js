20b3d141abfef9681b480fb1f123fcf80ecaa88a
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-11 01:25:01 -0400

    added collages section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Banners from './components/Banners.js'
 import Drawings from './components/Drawings.js'
 import Drawings1 from './components/Drawings1.js'
 import Paintings from './components/Paintings.js'
+import Collages from './components/Collages.js'
 import NavBar from './components/NavBar.js'
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path='/paintings' component={Paintings} />
           <Route exact path='/drawings/nose-1' component={Drawings1} />
           <Route exact path='/drawings' component={Drawings} />
+          <Route exact path='/collages' component={Collages} />
 
 
         </Switch>
---
+++ frontend/src/components/Collages.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../css/Collages.css"
+
+const Collages = (props) => {
+  return (
+    <div className='collages'>
+    <h1 className="collageScript"><img src={require("../media/script2/Collages.png")} alt='collagesimg'></img></h1>
+    <ul className="collageList">
+    <li><img src={require("../media/Collages/Dog Bride 1.JPG")} alt='drawing1'></img></li>
+    <li><img src={require("../media/Collages/Dog Bride 2.JPG")} alt='drawing2'></img></li>
+    <li><img src={require("../media/Collages/Dog Bride 3.JPG")} alt='drawing3'></img></li>
+    <li><img src={require("../media/Collages/Dog Bride 4.JPG")} alt='drawing4'></img></li>
+    </ul>
+    </div>
+  )
+}
+
+
+export default Collages;
