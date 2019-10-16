cb6d83ff27ee102ad30851c935eea609ba8be4d8
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-16 07:53:36 -0400

    banners script formatted

--- frontend/src/components/Banners.js
+++ frontend/src/components/Banners.js
@@ -4,13 +4,15 @@ import "../css/Banners.css"
 const Banners = (props) => {
   return (
     <div className='banners'>
-      <h1 className="bannersScript"><img src={require("../media/script2/Banners.png")} alt='about'></img></h1>
-      <img src={require("../media/Banners/Banner 1.jpg")} alt='banner1'></img>
-      <img src={require("../media/script/Banner 1 desc.png")} alt='banner2'></img>
-      <img src={require("../media/Banners/Banner 2.jpg")} alt='banner2'></img>
-      <img src={require("../media/script/Banner 2 desc.png")} alt='banner2'></img>
-      <img src={require("../media/Banners/Banner 3.jpg")} alt='banner3'></img>
-      <img src={require("../media/Banners/Banner 4.jpg")} alt='banner4'></img>
+      <div className="bannersScript"><img src={require("../media/script2/Banners.png")} alt='about'></img></div>
+    <ul className="bannerList">
+      <li><img src={require("../media/Banners/Banner 1.jpg")} alt='banner1'></img></li>
+      <li><img src={require("../media/script/Banner 1 desc.png")} alt='banner2'></img></li>
+      <li><img src={require("../media/Banners/Banner 2.jpg")} alt='banner2'></img></li>
+      <li><img src={require("../media/script/Banner 2 desc.png")} alt='banner2'></img></li>
+      <li><img src={require("../media/Banners/Banner 3.jpg")} alt='banner3'></img></li>
+      <li><img src={require("../media/Banners/Banner 4.jpg")} alt='banner4'></img></li>
+      </ul>
     </div>
   )
 }
