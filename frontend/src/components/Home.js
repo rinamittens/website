3bb256bc6e8ea539e80e8aabcae9fc13cfc1a410
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-15 23:54:05 -0400

    responsive img resizing

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,13 +10,13 @@ function Home() {
             <div className="paintHome">
               <Link to='/paintings'><img src={require("../media/script2/Paintings.png")} alt='about'></img></Link>
             </div>
-            <div>
+            <div className="drawHome">
               <Link to='/drawings'><img src={require("../media/script2/Drawings.png")} alt='about'></img></Link>
             </div>
-            <div>
+            <div className="bannerHome">
               <Link to='/banners'><img src={require("../media/script2/Banners.png")} alt='about'></img></Link>
             </div>
-            <div>
+            <div className="collageHome">
               <Link to='/collages'><img src={require("../media/script2/Collages.png")} alt='about'></img></Link>
             </div>
             <div className="aboutHome">
