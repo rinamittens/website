51e57e62dcb75db70f1877f91bcdf2f4bc3bcc41
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-23 17:04:08 -0400

    take 2

---
+++ build/precache-manifest.b2019ab3ae41841418f4c025a6aec952.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84803f5d25e5f02ec4dbcbd1dcc58653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3dbe7c150c9da6521d",
+    "url": "/static/css/main.5e83c968.chunk.css"
+  },
+  {
+    "revision": "f678ef5493bd6378131d",
+    "url": "/static/js/2.d3148e12.chunk.js"
+  },
+  {
+    "revision": "7b3dbe7c150c9da6521d",
+    "url": "/static/js/main.aee26062.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  },
+  {
+    "revision": "6aea0d48029e7fb879d8b269bf8f6ffa",
+    "url": "/static/media/1. Mountain.6aea0d48.jpg"
+  },
+  {
+    "revision": "b358e96b5625ffa269e778165329636e",
+    "url": "/static/media/1. Nose1.b358e96b.jpg"
+  },
+  {
+    "revision": "efe54cbb6576f1c4b112c50128c2ee9e",
+    "url": "/static/media/10. Pasta.efe54cbb.jpg"
+  },
+  {
+    "revision": "8293032f6cd423a643ff9e79c14191f8",
+    "url": "/static/media/11. Flowerama.8293032f.jpg"
+  },
+  {
+    "revision": "d5b7cc323c9e753faf7461b5da85fc98",
+    "url": "/static/media/2. Medusa.d5b7cc32.jpg"
+  },
+  {
+    "revision": "f590b0bb77f280359feac5faede56e0f",
+    "url": "/static/media/2. Nose2.f590b0bb.jpg"
+  },
+  {
+    "revision": "27a07dc0d129edded597dbe9d323a82a",
+    "url": "/static/media/3. Listening for the Sea.27a07dc0.jpg"
+  },
+  {
+    "revision": "e69b97ba4dd6108bc40f33e57faa0e85",
+    "url": "/static/media/3. Sweet 'n low installation shot.e69b97ba.jpg"
+  },
+  {
+    "revision": "d398cd2b223aea85545c89f8ee94c31f",
+    "url": "/static/media/4. Cookie Monster.d398cd2b.jpg"
+  },
+  {
+    "revision": "88dfcc4f807a6a783ecc522ae354a6ba",
+    "url": "/static/media/5. Worm Field.88dfcc4f.jpg"
+  },
+  {
+    "revision": "98521badcc1e6c912efae05e37e475f3",
+    "url": "/static/media/6. SweetnLow2.98521bad.JPG"
+  },
+  {
+    "revision": "4df27cd34bbf12164b9ae0186459e77f",
+    "url": "/static/media/6. SweetnLow3.4df27cd3.JPG"
+  },
+  {
+    "revision": "7f285a440e5541376cdfcc490f72f279",
+    "url": "/static/media/6. SweetnLow4.7f285a44.JPG"
+  },
+  {
+    "revision": "cb97a9f188502ee3cb6f46d0350d9dc3",
+    "url": "/static/media/6. SweetnLow5.cb97a9f1.JPG"
+  },
+  {
+    "revision": "f2bf338c4ce8a0b55b4a4d15f509b8f9",
+    "url": "/static/media/6. Two Columns.f2bf338c.jpg"
+  },
+  {
+    "revision": "89d98786808468e56e88b627cfbcbe3d",
+    "url": "/static/media/7. Artemis Triumphant.89d98786.jpg"
+  },
+  {
+    "revision": "be6fda2f0de38a35d58800995157d7c0",
+    "url": "/static/media/7. Diana diptych.be6fda2f.jpg"
+  },
+  {
+    "revision": "a4664aa732d47e5e5b5b02919c004708",
+    "url": "/static/media/8. Diana diptych 1.a4664aa7.jpg"
+  },
+  {
+    "revision": "9795c875f61950584ee2e68f3ce12ed4",
+    "url": "/static/media/8. Patience.9795c875.jpg"
+  },
+  {
+    "revision": "a7427c98947af59462978099e227aaa4",
+    "url": "/static/media/9. Artemis Window.a7427c98.jpg"
+  },
+  {
+    "revision": "ab901d3866e8a0246804955bda8dca20",
+    "url": "/static/media/9. Diana diptych 2.ab901d38.jpg"
+  },
+  {
+    "revision": "30494183021fa0b25aec3e4d02cf87ec",
+    "url": "/static/media/9. Jukay and His Mom.30494183.jpg"
+  },
+  {
+    "revision": "402ce88de584ef7c3718f6d5508e8d65",
+    "url": "/static/media/About.402ce88d.png"
+  },
+  {
+    "revision": "f52f47f93e61dae16bb1885e3225747c",
+    "url": "/static/media/Banner 1.f52f47f9.jpg"
+  },
+  {
+    "revision": "91c31502f172514d9486eeecbae50ca7",
+    "url": "/static/media/Banner 2.91c31502.jpg"
+  },
+  {
+    "revision": "bda0e2c7d9a616973cb09562268adb95",
+    "url": "/static/media/Banner 3.bda0e2c7.jpg"
+  },
+  {
+    "revision": "ee63a731b287070291be005d09f3923f",
+    "url": "/static/media/Banner 4.ee63a731.jpg"
+  },
+  {
+    "revision": "05e441e8a88934d6466dc0b592a67d6a",
+    "url": "/static/media/Banners.05e441e8.png"
+  },
+  {
+    "revision": "151482a7caed170b16bfec9b5e9d6fe9",
+    "url": "/static/media/BlessedPeace.151482a7.png"
+  },
+  {
+    "revision": "fbb43a0e52497605177156717416221f",
+    "url": "/static/media/Collages.fbb43a0e.png"
+  },
+  {
+    "revision": "e2635545035bd18df9a2d7e635952659",
+    "url": "/static/media/Dare.e2635545.png"
+  },
+  {
+    "revision": "40e03049b1446bc317c0cf721fc01271",
+    "url": "/static/media/Dog Bride 1.40e03049.JPG"
+  },
+  {
+    "revision": "d1663a2099c747df61c5d8f0c235ce33",
+    "url": "/static/media/Dog Bride 2.d1663a20.JPG"
+  },
+  {
+    "revision": "3453be60062b52ec831eccefbc3dc677",
+    "url": "/static/media/Dog Bride 3.3453be60.JPG"
+  },
+  {
+    "revision": "e29400fc6fc788e4b9db1974bc62634a",
+    "url": "/static/media/Dog Bride 4.e29400fc.JPG"
+  },
+  {
+    "revision": "7d6bea0128c77abbec3d0eded62ebeba",
+    "url": "/static/media/Drawings.7d6bea01.png"
+  },
+  {
+    "revision": "a6a0febb344641271c4860c8733258e2",
+    "url": "/static/media/ExpandBushMind.a6a0febb.png"
+  },
+  {
+    "revision": "a943dd5d5ca7e5748482e39b73cbbeba",
+    "url": "/static/media/Face_Square.a943dd5d.jpg"
+  },
+  {
+    "revision": "2a8fbb93ba926544567fe1c695ff7cd0",
+    "url": "/static/media/Instagram.2a8fbb93.png"
+  },
+  {
+    "revision": "976b562de4a2bef6d6c9473870fadb71",
+    "url": "/static/media/JusticeLove.976b562d.png"
+  },
+  {
+    "revision": "e238a6b952016ec302e26a0549fd1525",
+    "url": "/static/media/LinkedIn.e238a6b9.png"
+  },
+  {
+    "revision": "e4b5d19e23839a63bc187cd89aa025b9",
+    "url": "/static/media/Paintings.e4b5d19e.png"
+  },
+  {
+    "revision": "d08cfb0a72ad5798bed0c2f0c4787e6f",
+    "url": "/static/media/RLG.d08cfb0a.png"
+  },
+  {
+    "revision": "4eebbf61d560350e89eed846da8b0d73",
+    "url": "/static/media/RinaBio.4eebbf61.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
