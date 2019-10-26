e09f121b5e707af9c5fe162c4604df7356e8f4d7
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-26 05:17:50 -0400

    compressed drawings

--- src/components/Drawings.js
+++ src/components/Drawings.js
@@ -9,10 +9,10 @@ const Drawings = (props) => {
     <li className="needGap"><img src={require("../media/Drawings/1. Nose1.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/2. Nose2.jpg")} alt='drawing2'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/3. Sweet 'n low installation shot.jpg")} alt='drawing3'></img></li>
-    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow2.JPG")} alt='drawing4'></img></li>
-    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow3.JPG")} alt='drawing5'></img></li>
-    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow4.JPG")} alt='drawing1'></img></li>
-    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow5.JPG")} alt='drawing1'></img></li>
+    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow2.jpg")} alt='drawing4'></img></li>
+    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow3.jpg")} alt='drawing5'></img></li>
+    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow4.jpg")} alt='drawing1'></img></li>
+    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow5.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/7. Diana diptych.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/8. Diana diptych 1.jpg")} alt='drawing1'></img></li>
     <li><img src={require("../media/Drawings/9. Diana diptych 2.jpg")} alt='drawing1'></img></li>
