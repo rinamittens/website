351fc2fc7834579d3b4c381d382c407d596c4854
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-26 05:48:17 -0400

    sweetnlow bug test

--- src/components/Drawings.js
+++ src/components/Drawings.js
@@ -11,7 +11,7 @@ const Drawings = (props) => {
     <li className="needGap"><img src={require("../media/Drawings/3. Sweet 'n low installation shot.jpg")} alt='drawing3'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/4. SweetnLow2.jpg")} alt='drawing4'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/5. SweetnLow3.jpg")} alt='drawing5'></img></li>
-    <li className="needGap"><img src={require("../media/Drawings/6. SweetnLow4.jpg")} alt='drawing1'></img></li>
+    <li className="needGap"><img src={require("../media/Drawings/5.5. SweetnLow4.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/7. SweetnLow5.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/8. Diana diptych.jpg")} alt='drawing1'></img></li>
     <li className="needGap"><img src={require("../media/Drawings/9. Diana diptych 1.jpg")} alt='drawing1'></img></li>
