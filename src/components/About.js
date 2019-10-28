f1d9a319f05ed7a119e78dc0550f284c8da11ea9
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-27 23:32:52 -0400

    mobile about page complete

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,21 @@ const About = (props) => {
     <div className='about'>
       <div className='aboutChild'>
           <div className="aboutScript"><img src={require("../media/script2/About.png")} alt='about'></img></div>
-        <div className="bioPortrait">
+
             <div className="rinaPortrait">
               <img id="portrait" src={require("../media/Descriptions/Face_Square.jpg")} alt='facesquare'></img>
               <a href="https://www.instagram.com/rinagoldfish/?hl=en">
+              <div className="rinaBio"><img src={require("../media/Descriptions/RinaBio.png")} alt='bio' /></div>
+              <div className="mobileRinaBio">
+              <img src={require("../media/mobilebio/biopart1.png")} alt='bio' />
+              <img src={require("../media/mobilebio/biopart2.png")} alt='bio' />
+              <img src={require("../media/mobilebio/biopart3.png")} alt='bio' />
+              <img src={require("../media/mobilebio/biopart4.png")} alt='bio' />
+              </div>
               <img id="instagram" src={require("../media/Descriptions/Instagram.png")} alt='instagram'></img></a>
               <a href="https://www.linkedin.com/in/rina-goldfield-5a9068aa/">
               <img id="linkedin" src={require("../media/Descriptions/LinkedIn.png")} alt='linkedin'></img></a>
-            </div>
-            <div className="rinaBio"><img src={require("../media/Descriptions/RinaBio.png")} alt='bio' /></div>
+
         </div>
       </div>
     </div>
