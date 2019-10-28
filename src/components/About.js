47b4f404819c0ba0b6c59688239ecc3457c96c2c
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-28 00:54:16 -0400

    desktop and mobile about adjustments

--- src/components/About.js
+++ src/components/About.js
@@ -9,19 +9,21 @@ const About = (props) => {
 
             <div className="rinaPortrait">
               <img id="portrait" src={require("../media/Descriptions/Face_Square.jpg")} alt='facesquare'></img>
-              <a href="https://www.instagram.com/rinagoldfish/?hl=en">
-              <div className="rinaBio"><img src={require("../media/Descriptions/RinaBio.png")} alt='bio' /></div>
+              <img id="rinaBio" src={require("../media/Descriptions/RinaBio.png")} alt='bio' />
+            </div>
               <div className="mobileRinaBio">
               <img src={require("../media/mobilebio/biopart1.png")} alt='bio' />
               <img src={require("../media/mobilebio/biopart2.png")} alt='bio' />
               <img src={require("../media/mobilebio/biopart3.png")} alt='bio' />
               <img src={require("../media/mobilebio/biopart4.png")} alt='bio' />
               </div>
+              <div className="social">
+              <a href="https://www.instagram.com/rinagoldfish/?hl=en">
               <img id="instagram" src={require("../media/Descriptions/Instagram.png")} alt='instagram'></img></a>
               <a href="https://www.linkedin.com/in/rina-goldfield-5a9068aa/">
               <img id="linkedin" src={require("../media/Descriptions/LinkedIn.png")} alt='linkedin'></img></a>
+              </div>
 
-        </div>
       </div>
     </div>
   )
