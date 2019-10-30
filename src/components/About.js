42401fef7b6d322860d7b6e33f57b57e9aae0b50
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2019-10-30 01:58:51 -0400

    about css for tablet

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,17 @@ const About = (props) => {
               <img id="portrait" src={require("../media/Descriptions/Face_Square.jpg")} alt='facesquare'></img>
               <img id="rinaBio" src={require("../media/Descriptions/RinaBio.png")} alt='bio' />
             </div>
+            <div className="tabletPortraitBio">
+              <div className="tabletRinaPortrait">
+                <img id="portraitTablet" src={require("../media/Descriptions/Face_Square.jpg")} alt='facesquare'></img>
+              </div>
+              <div className="tabletRinaBio">
+                <img src={require("../media/mobilebio/biopart1.png")} alt='bio' />
+                <img src={require("../media/mobilebio/biopart2.png")} alt='bio' />
+                <img src={require("../media/mobilebio/biopart3.png")} alt='bio' />
+                <img src={require("../media/mobilebio/biopart4.png")} alt='bio' />
+              </div>
+            </div>
               <div className="mobileRinaBio">
               <img src={require("../media/mobilebio/biopart1.png")} alt='bio' />
               <img src={require("../media/mobilebio/biopart2.png")} alt='bio' />
