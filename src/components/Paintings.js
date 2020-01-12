e1942a57e9eb5302b7b9eab08bea471ccd6e527a
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2020-01-12 16:06:44 -0500

    new worm painting

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -11,6 +11,7 @@ const Paintings = (props) => {
       <li><img src={require("../media/Paintings/1a.medusalight.jpg")} alt='paint1'></img></li>
       <li><img src={require("../media/Paintings/2. Medusa.jpg")} alt='paint1'></img></li>
       <li><img src={require("../media/Paintings/3. Listening for the Sea.jpg")} alt='paint1'></img></li>
+      <li><img src={require("../media/Paintings/3a. Worm Pizza heart.jpg")} alt='paint1'></img></li>
       <li><img src={require("../media/Paintings/4. Cookie Monster.jpg")} alt='paint1'></img></li>
       <li><img src={require("../media/Paintings/5. Worm Field.jpg")} alt='paint1'></img></li>
       <li><img src={require("../media/Paintings/6. Two Columns.jpg")} alt='paint1'></img></li>
