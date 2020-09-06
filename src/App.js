3f9437462440f20f66c82745db86937415b87381
Evgeniya Ezhova <evgeniyaezhova@pursuit.org>
2020-09-06 18:16:01 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import logo from './logo.svg';
-import { Link } from 'react-router-dom'
 import Home from './components/Home.js'
 import About from './components/About.js'
 import Banners from './components/Banners.js'
 import Drawings from './components/Drawings.js'
-import Drawings1 from './components/Drawings1.js'
 import Paintings from './components/Paintings.js'
 import Collages from './components/Collages.js'
 import NavBar from './components/NavBar.js'
